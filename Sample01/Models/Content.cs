7ff49f2966143e80a6b74ab6cfffa7fdb500e120
shooter1217 <61668012@qq.com>
2020-03-30 18:42:09 +0800

    新增示例代码

---
+++ Sample01/Models/Content.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample01.Models
+{
+    public class Content
+    {
+        public int Id { set; get; }
+
+        public string title { set; get; }
+
+        public string content { set; get; }
+
+        public int status { set; get; }
+
+        public DateTime add_time { set; get; }
+
+        public DateTime modify_time { set; get; }
+
+    }
+}
---
+++ Sample01/Models/ContentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample01.Models
+{
+    public class ContentViewModel
+    {
+        public IList<Content> Contents { set; get; }
+    }
+}
